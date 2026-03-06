9945aeb1feef5148e880c8ab210719d880673a02
guozhongze <Alan.Zhao@imotion.ai>
2026-03-06 17:12:30 +0800

    cmake

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "testFunc.h"
+#include "testFunc2.h"
+
+int main(void)
+{
+	func(100);
+	func2(200);
+
+	return 0;
+}
---
+++ testFunc/testFunc.cpp
@@ -0,0 +1,9 @@
+
+
+#include <iostream>
+#include "testFunc.h"
+
+void func(int data)
+{
+	std::cout << "hello world, data:" << data << std::endl;
+}
